ac57bbb4789a4bee5973807721b9fb456607fde1
Sanchit <64640799+sanch1t@users.noreply.github.com>
2024-01-19 18:53:45 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ function Home() {
       <Parallax />
       <div className='about'>
         <h2>
-          Lorem ipsum
+          Placeholder
         </h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit laborum ipsam corrupti asperiores magnam quos cumque animi tempore vero repellendus, harum odio neque quis, non temporibus. Inventore asperiores repudiandae praesentium ut, fugit quo esse, placeat ullam quibusdam perspiciatis delectus ducimus nihil. Dolorum nam veniam aperiam sapiente corporis! Quisquam, veritatis repellendus?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit laborum ipsam corrupti asperiores magnam quos cumque animi tempore vero repellendus, harum odio neque quis, non temporibus. Inventore asperiores repudiandae praesentium more PLaceholder ut, fugit quo esse, placeat ullam quibusdam perspiciatis delectus ducimus nihil. Dolorum nam veniam aperiam sapiente corporis! Quisquam, veritatis repellendus?</p>
       </div>
     </>
   )
